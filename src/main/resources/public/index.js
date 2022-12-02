242dd60e8c376b9a988032a572a1006a84f307b7
Roger-Chavez <roger.chavez.03@gmail.com>
2022-12-02 15:12:32 -0800

    Refresh table on successful post request and clear text area

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -5,55 +5,57 @@ let getconfig = {
     },
 };
 
+function getGymData() {
 // Get Gym Data, parse, and create table
-fetch('https://gym-db-174.herokuapp.com/select', getconfig)
-    .then((response) => {
-        return response.json();
-    }).then(data => {
+    fetch('https://gym-db-174.herokuapp.com/select', getconfig)
+        .then((response) => {
+            return response.json();
+        }).then(data => {
 
-    // Creating Table
-    let table = document.createElement("table");
+        // Creating Table
+        let table = document.createElement("table");
 
-    // Creating Table header
-    let thread = table.createTHead();
-    let row = thread.insertRow();
-    let th = document.createElement("th");
-    th.appendChild(document.createTextNode("id"));
-    row.appendChild(th);
-    th = document.createElement("th");
-    th.appendChild(document.createTextNode("gym_name"));
-    row.appendChild(th);
-    th = document.createElement("th");
-    th.appendChild(document.createTextNode("location"));
-    row.appendChild(th);
+        // Creating Table header
+        let thread = table.createTHead();
+        let row = thread.insertRow();
+        let th = document.createElement("th");
+        th.appendChild(document.createTextNode("id"));
+        row.appendChild(th);
+        th = document.createElement("th");
+        th.appendChild(document.createTextNode("gym_name"));
+        row.appendChild(th);
+        th = document.createElement("th");
+        th.appendChild(document.createTextNode("location"));
+        row.appendChild(th);
 
-    row = thread.insertRow();
+        row = thread.insertRow();
 
-    let body = table.createTBody();
-    // Creating table rows for data
-    for (let key in data) {
-        // Add row to table
-        row = body.insertRow();
+        let body = table.createTBody();
+        // Creating table rows for data
+        for (let key in data) {
+            // Add row to table
+            row = body.insertRow();
 
-        // Go through each json object, stringify and parse
-        let jsondata = JSON.stringify(data[key]);
-        let obj = JSON.parse(jsondata);
+            // Go through each json object, stringify and parse
+            let jsondata = JSON.stringify(data[key]);
+            let obj = JSON.parse(jsondata);
 
-        // Insert a cell and set the value for each gym column
-        let cell = row.insertCell();
-        let ctext = document.createTextNode(obj["id"]);
-        cell.appendChild(ctext);
-        cell = row.insertCell();
-        ctext = document.createTextNode(obj["gymName"]);
-        cell.appendChild(ctext);
-        cell = row.insertCell();
-        ctext = document.createTextNode(obj["location"]);
-        cell.appendChild(ctext);
-    }
+            // Insert a cell and set the value for each gym column
+            let cell = row.insertCell();
+            let ctext = document.createTextNode(obj["id"]);
+            cell.appendChild(ctext);
+            cell = row.insertCell();
+            ctext = document.createTextNode(obj["gymName"]);
+            cell.appendChild(ctext);
+            cell = row.insertCell();
+            ctext = document.createTextNode(obj["location"]);
+            cell.appendChild(ctext);
+        }
 
-    // Appending built table to div with id=result
-    document.getElementById('result').appendChild(table);
-}).catch(err => console.log("err", err));
+        // Appending built table to div with id=result
+        document.getElementById('result').appendChild(table);
+    }).catch(err => console.log("err", err));
+}
 
 // Insert function, called when enter is clicked
 function insert() {
@@ -63,5 +65,7 @@ function insert() {
 
     fetch(url).then((data) => {
         console.log(data);
+        getGymData();
+        document.getElementById('input-text').value = '';
     }).catch(err => console.log("err", err));
 }
